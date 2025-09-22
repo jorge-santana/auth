e81a93fe8697ef948cfff62474c665fe10535baf
Jorge Sant Ana <jorge@argus-academy.com>
2025-09-22 09:51:02 -0300

    :tada: feat: Criando o schema de validação com Zod e React-Hook-Form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -5,8 +7,32 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod/v4";
+
+// Schema de validação
+const formSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+  passwordConfirm: z.string(),
+});
 
 export default function Page() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+  });
+
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    // Será executada no envio do form, apenas se os campos forem validados com base no schema
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Card className="w-full max-w-sm">
@@ -14,7 +40,11 @@ export default function Page() {
           <CardTitle>Registrar</CardTitle>
           <CardDescription>Crie sua conta, é simples e rápido!</CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}></form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   );
