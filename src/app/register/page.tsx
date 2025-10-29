dfdcae9092e2134a064277adb41ce8f4206b73a1
Jorge Sant Ana <jorge@argus-academy.com>
2025-10-29 11:42:06 -0300

    :tada: feat: Desabilitando o formulário durante a requisição e feedback de sucesso

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -22,6 +22,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { registerUser } from "./actions";
 import { userSchema, UserSchema } from "./schemas";
+import { resolve } from "path";
 
 export default function Page() {
   const form = useForm<UserSchema>({
@@ -51,69 +52,84 @@ export default function Page() {
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Registrar</CardTitle>
-          <CardDescription>Crie sua conta, é simples e rápido!</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(handleSubmit)}
-              className="flex flex-col gap-4"
-            >
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input {...field} type="email" />
-                    </FormControl>
-                    <FormDescription>
-                      Digite o seu melhor e-mail
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+      {form.formState.isSubmitSuccessful ? (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Conta registrada com sucesso</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Button>Login</Button>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Registrar</CardTitle>
+            <CardDescription>
+              Crie sua conta, é simples e rápido!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset
+                  className="flex flex-col gap-4"
+                  disabled={form.formState.isSubmitting}
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="email" />
+                        </FormControl>
+                        <FormDescription>
+                          Digite o seu melhor e-mail
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Senha</FormLabel>
-                    <FormControl>
-                      <Input {...field} type="password" />
-                    </FormControl>
-                    <FormDescription>
-                      A senha deve ter no mínimo 6 caracteres
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Senha</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" />
+                        </FormControl>
+                        <FormDescription>
+                          A senha deve ter no mínimo 6 caracteres
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-              <FormField
-                control={form.control}
-                name="passwordConfirm"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Confirmar senha</FormLabel>
-                    <FormControl>
-                      <Input {...field} type="password" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit">Registrar</Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+                  <FormField
+                    control={form.control}
+                    name="passwordConfirm"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Confirmar senha</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Registrar</Button>
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
