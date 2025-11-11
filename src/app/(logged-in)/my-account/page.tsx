6e43e14cc60953dad9bb0230d4a7902db56cff4f
Jorge Sant Ana <jorge@argus-academy.com>
2025-11-11 09:16:17 -0300

    :tada: feat: Criando a página de alteração de senha

--- src/app/(logged-in)/my-account/page.tsx
+++ src/app/(logged-in)/my-account/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <div>Minha conta</div>;
+import { auth } from "@/auth";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+
+export default async function Page() {
+  const session = await auth();
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>Minha conta</CardHeader>
+      <CardContent>
+        <Label>E-mail</Label>
+        <div className="text-muted-foreground">{session?.user?.email}</div>
+      </CardContent>
+    </Card>
+  );
 }
