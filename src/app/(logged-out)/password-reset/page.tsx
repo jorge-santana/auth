f57278780b4fe6eb83a892ad7c87e27a1eb7df21
Jorge Sant Ana <jorge@argus-academy.com>
2026-03-10 11:03:45 -0300

    feat: adiciona feedback de sucesso na solicitação de alteração de senha

--- src/app/(logged-out)/password-reset/page.tsx
+++ src/app/(logged-out)/password-reset/page.tsx
@@ -50,62 +50,74 @@ export default function Page() {
   };
   return (
     <div className="flex justify-center items-center min-h-screen">
-      {" "}
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Recuperação de senha</CardTitle>
-          <CardDescription>
-            Esqueceu sua senha? Sem problemas, informe o seu e-mail para
-            recuperá-la.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSubmit)}>
-              <fieldset
-                className="flex flex-col gap-4"
-                disabled={form.formState.isSubmitting}
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="email" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
+      {form.formState.isSubmitSuccessful ? (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Recuperação de senha</CardTitle>
+            <CardDescription>
+              Um link para redefinição de senha foi enviado para o e-mail
+              informado. Acesse sua caixa de entrada e clique no link para criar
+              uma nova senha.
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      ) : (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Recuperação de senha</CardTitle>
+            <CardDescription>
+              Esqueceu sua senha? Sem problemas, informe o seu e-mail para
+              recuperá-la.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset
+                  className="flex flex-col gap-4"
+                  disabled={form.formState.isSubmitting}
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="email" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <Button type="submit">Alterar senha</Button>
+                  {!!form.formState.errors.root?.message && (
+                    <FormMessage>
+                      {form.formState.errors.root?.message}
+                    </FormMessage>
                   )}
-                />
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="flex-col">
+            <div className="text-muted-foreground text-sm">
+              Ainda não tem conta?{" "}
+              <Link href="/register" className="underline">
+                Registre-se
+              </Link>
+            </div>
 
-                <Button type="submit">Alterar senha</Button>
-                {!!form.formState.errors.root?.message && (
-                  <FormMessage>
-                    {form.formState.errors.root?.message}
-                  </FormMessage>
-                )}
-              </fieldset>
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex-col">
-          <div className="text-muted-foreground text-sm">
-            Ainda não tem conta?{" "}
-            <Link href="/register" className="underline">
-              Registre-se
-            </Link>
-          </div>
-
-          <div className="text-muted-foreground text-sm">
-            Já tem uma conta?{" "}
-            <Link href="/login" className="underline">
-              Entarr
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+            <div className="text-muted-foreground text-sm">
+              Já tem uma conta?{" "}
+              <Link href="/login" className="underline">
+                Entrar
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      )}
     </div>
   );
 }
