98b309e0c9ffb0301af916bfaac10f385e63cbd8
Jorge Sant Ana <jorge@argus-academy.com>
2026-03-20 13:31:46 -0300

    feat: cria e armazena o segredo que será utilizado para geração do otp

--- src/app/(logged-in)/my-account/components/actions.ts
+++ src/app/(logged-in)/my-account/components/actions.ts
@@ -3,6 +3,7 @@
 import { auth } from "@/auth";
 import { prisma } from "@/lib/client";
 import { ActionResponse } from "@/types/action-response";
+import { generateSecret, generateURI } from "otplib";
 
 export const get2faSecret = async (): Promise<ActionResponse> => {
   const session = await auth();
@@ -23,10 +24,25 @@ export const get2faSecret = async (): Promise<ActionResponse> => {
     };
   }
 
-  //TODO: Gerar e salvar o segredo para 2FA.
+  let twoFactorSecret = user.twoFactorSecret;
+
+  if (!twoFactorSecret) {
+    twoFactorSecret = generateSecret();
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { twoFactorSecret },
+    });
+  }
+
+  const uri = generateURI({
+    issuer: process.env.APP_NAME || "",
+    label: user.email,
+    secret: twoFactorSecret,
+  });
 
   return {
     message: "Chegamos até o método get2faSecret",
     success: true,
+    data: uri,
   };
 };
