7020c44c9449ee576b5b957afcbed0e3d01bbce9
Jorge Sant Ana <jorge@argus-academy.com>
2026-03-26 09:17:57 -0300

    feat: adiciona formulário de preenchimento do otp (segundo step do fluxo de login)

--- src/app/(logged-out)/login/page.tsx
+++ src/app/(logged-out)/login/page.tsx
@@ -24,8 +24,19 @@ import { useForm } from "react-hook-form";
 import { loginWithCredentials, preLoginCheck } from "./action";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useState } from "react";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
 
 export default function Page() {
+  const [step, setStep] = useState(1);
+  const [otp, setOtp] = useState("");
+
   const router = useRouter();
   const form = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
@@ -51,6 +62,7 @@ export default function Page() {
     if (twoFactorActivated) {
       // TODO: exibir o formulário para inserir o OTP
       console.log("exibir o formulário para inserir o OTP");
+      setStep(2);
     } else {
       const response = await loginWithCredentials(data);
       if (!response.success) {
@@ -70,75 +82,127 @@ export default function Page() {
       }
     }
   };
+
+  const handleOTPSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(
+      "Acionar a server action para validar o OTP e seguir ou não com o fluxo de login",
+    );
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen">
-      {" "}
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>Acesse sua conta.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSubmit)}>
-              <fieldset
-                className="flex flex-col gap-4"
-                disabled={form.formState.isSubmitting}
+      {step === 1 && (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Acesse sua conta.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset
+                  className="flex flex-col gap-4"
+                  disabled={form.formState.isSubmitting}
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="email" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Senha</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="password" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <Button type="submit">Entrar</Button>
+                  {!!form.formState.errors.root?.message && (
+                    <FormMessage>
+                      {form.formState.errors.root?.message}
+                    </FormMessage>
+                  )}
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="flex-col">
+            <div className="text-muted-foreground text-sm">
+              Ainda não tem conta?{" "}
+              <Link href="/register" className="underline">
+                Registre-se
+              </Link>
+            </div>
+
+            <div className="text-muted-foreground text-sm">
+              Esqueceu sua senha?{" "}
+              <Link href="/password-reset" className="underline">
+                Alterar senha
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      )}
+
+      {step === 2 && (
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>OTP (one-time password)</CardTitle>
+            <CardDescription>
+              Entre com o OTP exibido no seu App de Autenticação.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleOTPSubmit} className="flex flex-col gap-2">
+              <InputOTP
+                maxLength={6}
+                pattern={REGEXP_ONLY_DIGITS}
+                value={otp}
+                onChange={setOtp}
               >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="email" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Senha</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="password" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <Button type="submit">Entrar</Button>
-                {!!form.formState.errors.root?.message && (
-                  <FormMessage>
-                    {form.formState.errors.root?.message}
-                  </FormMessage>
-                )}
-              </fieldset>
+                <InputOTPGroup>
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                </InputOTPGroup>
+                <InputOTPSeparator />
+                <InputOTPGroup>
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+              <Button disabled={otp.length !== 6}>Verificar</Button>
+              <Button
+                variant={"outline"}
+                onClick={() => {
+                  form.setValue("password", "");
+                  setStep(1);
+                }}
+              >
+                Cancelar
+              </Button>
             </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex-col">
-          <div className="text-muted-foreground text-sm">
-            Ainda não tem conta?{" "}
-            <Link href="/register" className="underline">
-              Registre-se
-            </Link>
-          </div>
-
-          <div className="text-muted-foreground text-sm">
-            Esqueceu sua senha?{" "}
-            <Link href="/password-reset" className="underline">
-              Alterar senha
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
